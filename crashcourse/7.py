92fb66178e650e10626f881a723810952a3d8500
Aadarsh <aadarsh-s@outlook.com>
2020-09-14 20:42:11 +0530

    STRING METHODS

---
+++ crashcourse/7.py
@@ -0,0 +1,6 @@
+# STRING METHODS
+text = ' Hello.mofos.this.is.aadarsh.here'
+print(text.strip())  # Removes spaces from the beginning of the string
+print(text.lower())  # Removes all the caps from the string
+print(text.upper())  # Makes all the letter in the string CAPITAL
+print(text.split('.'))  # Splits the string wrt '.' and puts them into a list

d062ec7898b9c18639bd4f72e19c8dc78fbe6e63
Aadarsh <aadarsh-s@outlook.com>
2020-09-14 11:31:14 +0530

    Conditions

---
+++ crashcourse/2.py
@@ -0,0 +1,14 @@
+# Conditions
+numDic = {'num1': 20, 'num2': 3,
+          'str1': 'Hello Mofos!', 'str2': 'hello mofos!'}
+
+print('Printing Value \n')
+print('NUM 1', numDic['num1'])
+print('NUM 2', numDic['num2'])
+print('STR 1', numDic['str1'])
+print('STR 2', numDic['str2'])
+
+print('\nGreater than : ', numDic['num1'] > numDic['num2'])
+print('Less than : ', numDic['num1'] < numDic['num2'])
+print('Equal : ', numDic['num1'] == numDic['num2'])
+print('Not Equal : ', numDic['str1'] != numDic['str2'])

6f459ca894dcce727f0f597e521595a2109b488a
Aadarsh <aadarsh-s@outlook.com>
2020-09-14 21:14:44 +0530

    BASIC LIST METHODS

---
+++ crashcourse/9.py
@@ -0,0 +1,9 @@
+# LIST METHOD
+
+arr = ['Windows', 'Mac', 'Linux', 'aksdSAKSJHDAkjhdakjsd']
+
+# Prints the index of 'n' in the string / If letter not found it returns -1
+print('.find() =>', arr[2].find('n'))
+
+# Prints the number of times the string has repeated
+print('.count() =>', arr[3].lower().count('a'))

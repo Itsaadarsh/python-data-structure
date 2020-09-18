16b9358672e9c0ddbf41637792f8814b9ed66e27
Aadarsh <aadarsh-s@outlook.com>
2020-09-19 02:01:32 +0530

    Manually creating n elements and inserting them at a given index

--- linkedList/8.py
+++ linkedList/8.py
@@ -1,4 +1,5 @@
 # Creating n elements in a linkedlist
+# Manually creating n elements and inserting them at a given index
 
 class Node:
     def __init__(self, data):
@@ -34,9 +35,42 @@ class LinkedList:
             data = input('Enter the data for NODE {} : '.format(i+1))
             self.pushingElements(data)
 
+    def insertingAtAIndex(self):
+
+        data = input('Enter the data for NEWNODE : ')
+        index = int(input('Enter the index : '))
+        temp = self.head
+        ind = 0
+        while(temp):
+            if(ind == index):
+                newNode = Node(data)
+                newNode.next = temp
+                temp = newNode
+                self.head = temp
+                break
+            ind += 1
+            if (ind == index):
+                newNode = Node(data)
+                newNode.next = temp.next
+                temp.next = newNode
+                break
+            temp = temp.next
+
+        if(temp is None):
+            print('Item Not found at index {}!'.format(index))
+        else:
+            self.display()
+
 
 if __name__ == "__main__":
 
     llist = LinkedList()
+
+    # Creating n elements
     llist.creating()
+
+    # Displaying those n elements
     llist.display()
+
+    # Inserting an element at a given index
+    llist.insertingAtAIndex()

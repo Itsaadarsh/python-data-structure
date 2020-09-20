f87ac592378553b45b24203960cbda13345af7aa
Aadarsh <aadarsh-s@outlook.com>
2020-09-20 22:41:34 +0530

    Creating node

---
+++ stack/2.py
@@ -0,0 +1,14 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+
+class Stack:
+    def __init__(self):
+        self.head = None
+
+
+if __name__ == "__main__":
+
+    newStack = Stack()

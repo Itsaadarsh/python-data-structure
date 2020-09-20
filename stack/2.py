289e9228ed64729f0bc02645b074eead9593372b
Aadarsh <aadarsh-s@outlook.com>
2020-09-21 00:20:55 +0530

    Stack Implementation using LinkedList

--- stack/2.py
+++ stack/2.py
@@ -1,3 +1,5 @@
+# Stack Implementation using LinkedList
+
 class Node:
     def __init__(self, data):
         self.data = data
@@ -43,3 +45,33 @@ class Stack:
 
         else:
             print("\n\nTOP element in the stack is {}".format(self.top.data))
+
+
+if __name__ == "__main__":
+
+    newStack = Stack()
+    choice = '-1'
+    while(choice != '4'):
+        print("\n\n------------------------------------\n")
+        print("    STACK IMPLEMENTATION PROGRAM    \n")
+        print("------------------------------------\n")
+        print("1. Push\n")
+        print("2. Pop\n")
+        print("3. Peek\n")
+        print("4. Exit\n")
+        print("5. Display Elements\n")
+        print("------------------------------------\n")
+        choice = (input("Enter your choice: "))
+
+        if(choice == '1'):
+            newStack.pushing()
+        elif(choice == '2'):
+            newStack.popping()
+        elif(choice == '3'):
+            newStack.peak()
+        elif(choice == '4'):
+            choice = '4'
+        elif(choice == '5'):
+            newStack.display()
+        else:
+            print('Invalid INPUT! Try again')

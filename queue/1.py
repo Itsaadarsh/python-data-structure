4889fd37eda6c4da4d2d8e27bf88645d9f1ea5c3
Aadarsh <aadarsh-s@outlook.com>
2020-09-23 06:12:56 +0530

    BASIC QUEUE OPERATIONS

--- queue/1.py
+++ queue/1.py
@@ -1,1 +1,18 @@
 # Basic queue operation
+
+queue = []
+
+queue.append('a')
+queue.append('b')
+queue.append('c')
+
+print("Initial queue")
+print(queue)
+
+print("\nElements dequeued from queue")
+print(queue.pop(0))
+print(queue.pop(0))
+print(queue.pop(0))
+
+print("\nQueue after removing elements")
+print(queue)

c45c078afcc8a8f391cd9c6c0aaacc56b9fd1789
Aadarsh <aadarsh-s@outlook.com>
2020-09-24 16:44:42 +0530

    adding full & empty

--- queue/1.py
+++ queue/1.py
@@ -3,5 +3,11 @@ class Queue:
     def __init__(self, capacity):
         self.front = self.size = 0
         self.rear = capacity - 1
-        self.Q = [None]*capacity
+        self.queue = [None]*capacity
         self.capacity = capacity
+
+    def isFull(self):
+        return self.size == self.capacity
+
+    def isEmpty(self):
+        return self.size == 0

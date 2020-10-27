043f4349f752a484674db449c5139da1b4d6ba72
Aadarsh <aadarsh-s@outlook.com>
2020-10-27 20:04:18 +0530

    Insertion done

--- trees/1.py
+++ trees/1.py
@@ -15,11 +15,25 @@ def displayTree(temp):
     displayTree(temp.right)
 
 
-if __name__ == "__main__":
-    tree = Node(1)
-    tree.left = Node(2)
-    tree.left.left = Node(4)
-    tree.right = Node(3)
-    tree.right.right = Node(5)
+def insert(parent, data):
 
+    if(parent is None):
+        return Node(data)
+
+    else:
+        if(parent.key == data):
+            return parent
+        elif(parent.key < data):
+            parent.right = insert(parent.right, data)
+        else:
+            parent.left = insert(parent.left, data)
+    return parent
+
+
+if __name__ == "__main__":
+    tree = Node(3)
+    insert(tree, 1)
+    insert(tree, 2)
+    insert(tree, 4)
+    insert(tree, 0)
     displayTree(tree)
